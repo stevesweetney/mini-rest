d5cee311ad7bdcda1c5a6279c918e4ae22ef2ce8
Steve Sweetney <stevesweetney@gmail.com>
2023-02-22 03:13:35 -0500

    Add clap - Add clap to parse command line arguments - Update parameters to be more descriptive

--- src/looping.rs
+++ src/looping.rs
@@ -13,23 +13,31 @@ fn prepare_chime<'a>() -> rodio::Decoder<Cursor<&'a [u8]>> {
     Decoder::new(cursor).expect("Chime SFX is not a valid audio file")
 }
 
-pub fn loop_breaks(start_after_seconds: u64) {
+pub fn loop_breaks(
+    start_after_seconds: u64,
+    min_work_time: u64,
+    max_work_time: u64,
+    rest_time: u64,
+) {
     let (_stream, stream_handle) =
         OutputStream::try_default().expect("Failed to open default audio output stream");
 
     let mut rng = thread_rng();
 
     thread::sleep(Duration::from_secs(start_after_seconds));
+    println!("Starting timer!");
 
     loop {
         stream_handle
             .play_raw(prepare_chime().convert_samples())
             .expect("Failed to play chime SFX");
-        thread::sleep(Duration::from_secs(10));
+        thread::sleep(Duration::from_secs(rest_time));
         stream_handle
             .play_raw(prepare_chime().convert_samples())
             .expect("Failed to play chime SFX");
 
-        thread::sleep(Duration::from_secs(150 + rng.gen_range(0..=180)));
+        thread::sleep(Duration::from_secs(
+            rng.gen_range(min_work_time..=max_work_time),
+        ));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,35 @@
+use clap::Parser;
+
 mod looping;
 
-fn main() {
-    println!("Starting timer!");
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Time in seconds to rest
+    #[arg(short, long, default_value_t = 10)]
+    rest_time: u64,
 
-    looping::loop_breaks(300);
+    /// Start timer after N seconds
+    #[arg(short, long, default_value_t = 300)]
+    start_n: u64,
+
+    /// Minimum time in seconds to work.
+    /// Lower bound for random work time
+    #[arg(short, long, default_value_t = 150)]
+    min_work_time: u64,
+
+    /// Maximum time in seconds to work.
+    /// Upper bound for random work time
+    #[arg(short, long, default_value_t = 270)]
+    max_work_time: u64,
+}
+
+fn main() {
+    let args = Args::parse();
+    looping::loop_breaks(
+        args.start_n,
+        args.min_work_time,
+        args.max_work_time,
+        args.rest_time,
+    );
 }
